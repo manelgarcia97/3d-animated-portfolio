9430b6214821b63f7334e4282e499671e6a83bff
Manel Garcia <39560269+manelgarcia97@users.noreply.github.com>
2025-03-11 21:16:12 +0100

    end of day one

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
 const App = () => {
   return (
+<<<<<<< Updated upstream
     <div className=''>Hello World</div>
+=======
+    <div className='container'>
+      <section id="#home">
+        <Hero />
+      </section>
+      <section id="#services">
+        <Services />
+      </section>
+      <section id="#portfolio">
+        <Portfolio />
+      </section>
+      <section id="#contact">
+        <Contact />
+      </section>      
+    </div>
+>>>>>>> Stashed changes
   )
 }
 
